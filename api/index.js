ac2b129e96a16c08b2710273f0c0a184e9d016d9
Kunwar609 <kunwargur3@gmail.com>
2024-07-25 14:51:02 +0530

    connect to DB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,21 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+console.log('MongoDB URI:', process.env.MONGODB_URI);
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error.message);
+  });
 
 const app = express();
 
 app.listen(3010, () => {
-    console.log('Server is running on port 3010!');
-})
+  console.log("Server is running on port 3010!");
+});
